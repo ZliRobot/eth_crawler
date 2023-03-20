0ad924ec76ca41060de5a76f6c9ca0cf3b778995
ZliRobot <dusanradovan@gmail.com>
2023-03-20 22:20:58 +0100

    Added functionality: get balance at the given time

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,16 @@ pub const RPC_URL: &str = "https://eth.llamarpc.com";
 
 #[derive(Parser)]
 pub struct Args {
-    // Address whose history you want to see.
+    /// Address whose history you want to see.
     pub address: Address,
-    // Block number from which you want to see the history.
-    pub starting_block: u64,
+
+    /// Block number starting from which you want to see the history.
+    #[arg(short, long)]
+    pub starting_block: Option<u64>,
+
+    /// Time at which you want to see the balance.
+    #[arg(short, long)]
+    pub time: Option<String>,
 }
 
 pub fn display_header() {
@@ -47,17 +53,23 @@ pub fn print_formated(transaction: Transaction, target: Address) {
 #[async_trait]
 pub trait EthCrawler {
     async fn transations_of_since_upto(
-        self,
+        &self,
         from: H160,
         since_block_number: u64,
         upto_block_number: u64,
     ) -> Result<Vec<Transaction>, ProviderError>;
+
+    async fn balance_at_timestamp(
+        &self,
+        address: H160,
+        time: i64,
+    ) -> Result<U256, ProviderError>;
 }
 
 #[async_trait]
 impl<P: JsonRpcClient> EthCrawler for Provider<P> {
     async fn transations_of_since_upto(
-        self,
+        &self,
         target_addr: H160,
         since_block_number: u64,
         upto_block_number: u64,
@@ -76,5 +88,39 @@ impl<P: JsonRpcClient> EthCrawler for Provider<P> {
             }
         }
         Ok(res)
+    }
+
+    async fn balance_at_timestamp(
+        &self,
+        address: H160,
+        time: i64,
+    ) -> Result<U256, ProviderError> {
+        let target_time = time as u64;
+        let mut upper_block = self.get_block_number().await?;
+        let mut lower_block: U64 = 0.into();
+        let mut current_block = (upper_block + lower_block) / 2;
+        let mut current_block_time = self
+            .get_block(current_block)
+            .await?
+            .ok_or_else(|| ProviderError::CustomError("Block unavailable".into()))?
+            .timestamp
+            .as_u64();
+
+        while upper_block > lower_block + U64::from(1) {
+            if current_block_time > target_time {
+                upper_block = current_block;
+            } else {
+                lower_block = current_block;
+            }
+            current_block = (lower_block + upper_block) / 2;
+            current_block_time = self
+                .get_block(current_block)
+                .await?
+                .ok_or_else(|| ProviderError::CustomError("Block unavailable".into()))?
+                .timestamp
+                .as_u64();
+        }
+
+        Ok(self.get_balance(address, Some(lower_block.into())).await?)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use chrono::DateTime;
 use clap::Parser;
 use ethers::prelude::*;
 
@@ -9,15 +10,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     let provider = Provider::<Http>::try_from(RPC_URL)?;
-    let current_block = provider.get_block_number().await?;
-    display_header();
 
-    for transaction in provider
-        .transations_of_since_upto(args.address, args.starting_block, current_block.as_u64())
-        .await?
-        .into_iter()
-    {
-        print_formated(transaction, args.address);
+    if let Some(starting_block) = args.starting_block {
+        let current_block = provider.get_block_number().await?;
+        display_header();
+        for transaction in provider
+            .transations_of_since_upto(args.address, starting_block, current_block.as_u64())
+            .await?
+            .into_iter()
+        {
+            print_formated(transaction, args.address);
+        }
+    }
+
+    if let Some(time) = args.time {
+        let time = DateTime::parse_from_str(&time, "%Y-%m-%d %H:%M:%S %z")?.timestamp();
+        println!(
+            "Balance at given time: {:?}",
+            provider.balance_at_timestamp(args.address, time).await?
+        );
     }
 
     Ok(())
