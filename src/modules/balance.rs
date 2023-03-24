e79be182095963db976462b6b5772fd7ff718843
ZliRobot <dusanradovan@gmail.com>
2023-03-24 14:23:23 +0100

    Retry when getting balance too

--- src/modules/balance.rs
+++ src/modules/balance.rs
@@ -1,3 +1,4 @@
+use super::RETRY_COUNT;
 use async_trait::async_trait;
 use ethers::prelude::*;
 use std::convert::From;
@@ -32,6 +33,8 @@ pub trait EthCrawlerBalance {
         address: H160,
         time: i64,
     ) -> Result<Balance, ProviderError>;
+
+    async fn get_block_time(&self, block_id: U64) -> Result<u64, ProviderError>;
 }
 
 #[async_trait]
@@ -45,13 +48,9 @@ impl<P: JsonRpcClient> EthCrawlerBalance for Provider<P> {
         let mut upper_block = self.get_block_number().await?;
         let mut lower_block: U64 = 0.into();
         let mut current_block = (upper_block + lower_block) / 2;
-        let mut current_block_time = self
-            .get_block(current_block)
-            .await?
-            .ok_or_else(|| ProviderError::CustomError("Block unavailable".into()))?
-            .timestamp
-            .as_u64();
+        let mut current_block_time = self.get_block_time(current_block).await?;
 
+        // Find the required block using binary search
         while upper_block > lower_block + U64::from(1) {
             if current_block_time > target_time {
                 upper_block = current_block;
@@ -59,17 +58,31 @@ impl<P: JsonRpcClient> EthCrawlerBalance for Provider<P> {
                 lower_block = current_block;
             }
             current_block = (lower_block + upper_block) / 2;
-            current_block_time = self
-                .get_block(current_block)
-                .await?
-                .ok_or_else(|| ProviderError::CustomError("Block unavailable".into()))?
-                .timestamp
-                .as_u64();
+            current_block_time = self.get_block_time(current_block).await?;
         }
 
         Ok(Balance {
             wei: self.get_balance(address, Some(lower_block.into())).await?,
         })
+    }
+
+    async fn get_block_time(&self, block_id: U64) -> Result<u64, ProviderError> {
+        let mut block = self.get_block(block_id).await;
+
+        // Repeat if there is a network error
+        let mut attempt = 0;
+        while let Err(ProviderError::JsonRpcClientError(_)) = block {
+            if attempt == RETRY_COUNT {
+                break;
+            }
+            attempt += 1;
+            block = self.get_block(block_id).await;
+        }
+
+        Ok(block?
+            .ok_or_else(|| ProviderError::CustomError("Block unavailable".into()))?
+            .timestamp
+            .as_u64())
     }
 }
 
